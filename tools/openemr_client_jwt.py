41dc864418ece9bbcc27f9b5bbda29146b41c37d
Prashant Anantharaman <lists@prashant.at>
2024-04-10 22:53:17 -0400

    Complete key generation in OpenEMR client

--- tools/openemr_client_jwt.py
+++ tools/openemr_client_jwt.py
@@ -9,9 +9,12 @@ and send FHIR API requests using them.
 """
 import random
 import string
+import json
 import requests
 import jwt
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from jwcrypto import jwk
+import click
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -20,22 +23,23 @@ class OpenEMRClient:
     """Create an OpenEMR client using the necessary JWT values
 
     Prerequisites: you must have a keypair (RSA private and public keys)
-    stored in id_rsa.pub and id_rsa files in the same folder.
+    stored in keypair_set, private_key and public_key files in the same folder.
 
-    You will also need to extract the kid from the https://mkjwk.org/ output
-    for the Keypair sets.
     """
 
-    def __init__(self, kid: str, client_id: str, hostname: str):
+    def __init__(self, client_id: str, hostname: str):
         """Constructor"""
         self.hostname = hostname
-        with open("id_rsa.pub", encoding="utf8") as f_pubfile:
-            self.public_key = f_pubfile.read()
 
-        with open("id_rsa", encoding="utf8") as f_privfile:
+        with open("keypair_set", encoding="utf8") as key_file:
+            self.keypair_set = json.loads(key_file.read())
+
+        with open("private_key", "rb") as f_privfile:
             self.private_key = f_privfile.read()
 
-        self.headers = {"alg": "RS384", "typ": "JWT", "kid": kid}
+        self.kid = self.keypair_set["keys"][0]["kid"]
+
+        self.headers = {"alg": "RS384", "typ": "JWT", "kid": self.kid}
         self.payload = {
             "sub": client_id,
             "iss": client_id,
@@ -78,7 +82,10 @@ class OpenEMRClient:
     def get_fhir_patients(self):
         """Request patients from the FHIR API using the access token"""
         assert self.access_token is not None
-        headers = {"Authorization": f"Bearer {self.access_token}"}
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-type": "xml",
+        }
         r = requests.get(
             f"{self.hostname}/apis/default/fhir/Patient",
             headers=headers,
@@ -91,7 +98,9 @@ class OpenEMRClient:
             raise Exception("Patient Access Failed")
 
     def export_all_fhir_patients(self):
-        """Bulk export option: https://github.com/openemr/openemr/blob/master/FHIR_README.md#bulk-fhir-exports"""
+        """Bulk export option:
+        https://github.com/openemr/openemr/blob/master/FHIR_README.md#bulk-fhir-exports
+        """
         assert self.access_token is not None
         headers = {"Authorization": f"Bearer {self.access_token}"}
         r = requests.get(
@@ -105,6 +114,45 @@ class OpenEMRClient:
         else:
             raise Exception("Patient Access Failed")
 
+def generate_keypair():
+    """
+    Generate keypairs using the jwk library
+    Creates/overwrites three files
+    """
+    kid = "".join(random.choices(string.ascii_uppercase + string.digits, k=20))
+    key = jwk.JWK.generate(kty="RSA", size=2048, alg="RS384", use="sig", kid=kid)
+    public_key = key.export_public()
+    private_key = key.export_private()
+    keypair_set = {"keys": [eval(private_key)]}
+    print(public_key, private_key, key.export_to_pem(True, None))
+    with open("keypair_set", "w", encoding="utf8") as f_keypair:
+        f_keypair.write(json.dumps(keypair_set))
+    with open("private_key", "wb") as priv:
+        priv.write(key.export_to_pem(True, None))
+    with open("public_key", "wb") as pub:
+        pub.write(key.export_to_pem(False, None))
+
+@click.command()
+@click.option("--clientid", required=True, help="Get Client ID cli argument")
+@click.option("--url", required=True, help="Get URL of OpenEMR cli argument")
+@click.option("--generate/--no-generate", default=False)
+def create_openemr_client(clientid, url, generate):
+    """
+    Defines the command-line interface used by this tool
+    Performs two tasks: generates a new keypair
+    This new keypair cannot be used right away as the keypair_set needs
+    to be pasted on the registration UI on OpenEMR
+    Grabs the clientID as an argument and tries to construct useful API queries
+    """
+    if generate:
+        generate_keypair()
+
+    client = OpenEMRClient(
+        clientid,
+        url,
+    )
+    client.get_authorization_token()
+    client.get_fhir_patients()
 
 def main():
     """Create an instance of the client and
@@ -115,17 +163,12 @@ def main():
 
     The access token is only usable for 300 seconds, and usable only once.
     """
-    client = OpenEMRClient(
-        "E6hXJBZXSh0FkmwUqQovT0vZSbKR-sr2WEPIV0b-joI",
-        "dbmJyFsKgNh59JLh3fx0R48HKY2UO043Z3UfitIpKoE",
-        "https://localhost",
-    )
-    client.get_authorization_token()
-    client.get_fhir_patients()
 
-    # Token has expired, generate one again
-    client.get_authorization_token()
-    client.export_all_fhir_patients()
+    create_openemr_client()
+
+    ## Token has expired, generate one again
+    # client.get_authorization_token()
+    # client.export_all_fhir_patients()
 
 
 if __name__ == "__main__":
