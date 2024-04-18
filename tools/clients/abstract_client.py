1702cef47990b74a9bb4fb1f64ded6200bffff64
Prashant Anantharaman <lists@prashant.at>
2024-04-18 19:57:45 -0400

    Add ABC and modified clients

---
+++ tools/clients/abstract_client.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+"""
+Define an abstract class to all the other clients have the same set of methods
+"""
+from abc import ABCMeta, abstractmethod
+
+
+class AbstractClient(metaclass=ABCMeta):
+    """Ensure that these methods are implemented in each implementation"""
+    @abstractmethod
+    def __init__(self, fhir, base):
+        """Constructor to ensure that we get the FHIR URL and API base URL"""
+
+    @abstractmethod
+    def export_patients(self):
+        """Extracts Patient data using a FHIR output"""
+    @abstractmethod
+    def create_patient(self, file):
+        """A function that creates a user by sending a FHIR JSON object or uploading a CCDA file"""
+
+    @abstractmethod
+    def export_patients(self):
+        """Extracts Patient data using a FHIR output"""
--- tools/clients/vista_client.py
+++ tools/clients/vista_client.py
@@ -8,22 +8,24 @@ Create a Client for vista that can create patients and pull data
 
 import click
 import requests
+from abstract_client import AbstractClient
 
 
-class HapiClient:
+class VistaClient(AbstractClient):
     """Allow users to easy create a new patient and export all patients"""
 
     def __init__(self, fhir, base):
         """Constructor"""
+        self.vehu = "http://localhost:9080"
         self.fhir = fhir
         self.base = base
 
-    def export(self):
+    def export_patients(self):
         """Calls the FHIR API to export all patients"""
         r = requests.get(f"{self.fhir}/{self.base}/Patient", timeout=100)
         return r
 
-    def create(self, file):
+    def create_patient(self, file):
         """Calls the MUMPS API to create a new patient from a FHIR JSON"""
         r = requests.post(f"{self.vehu}/addpatient", data=file.read(), timeout=10)
         return r.status_code
@@ -37,18 +39,16 @@ def cli_options(create, file):
     Extract command-line arguments to either create a new patient
     No arguments: exports all patients in a JSON form
     """
-    client = HapiClient("http://localhost:8004", "fhir")
+    client = VistaClient("http://localhost:8002", "api")
     if not create:
-        response = client.export()
+        response = client.export_patients()
         if response.status_code == 200:
             print(response.json())
-        else:
-            print(response.status_code)
     else:
         if file is None:
             print("Needs the --file argument")
         else:
-            print(client.create(file))
+            print(client.create_patient(file))
 
 
 if __name__ == "__main__":
