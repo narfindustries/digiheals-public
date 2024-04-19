fb89998756d9bc4964745908be48bf3f9cd6c463
Prashant Anantharaman <lists@prashant.at>
2024-04-19 13:07:19 -0400

    First pass at the game of telephone for vista -> hapi -> ibm fhir

--- tools/clients/ibm_fhir_client.py
+++ tools/clients/ibm_fhir_client.py
@@ -21,35 +21,73 @@ class IBMFHIRClient(AbstractClient):
 
     def export_patients(self):
         """Calls the FHIR API to export all patients"""
-        r = requests.get(f"{self.fhir}/{self.base}/Patient", timeout=100, verify=False, auth=("fhiruser", "change-password"))
+        r = requests.get(
+            f"{self.fhir}/{self.base}/Patient",
+            timeout=100,
+            verify=False,
+            auth=("fhiruser", "change-password"),
+        )
         return r
 
-    def create_patient(self, file):
+    def export_patient(self, p_id):
+        """Calls the FHIR API to export all patients"""
+        r = requests.get(
+            f"{self.fhir}/{self.base}/Patient/{p_id}",
+            timeout=100,
+            verify=False,
+            auth=("fhiruser", "change-password"),
+        )
+        return r
+
+    def create_patient_fromfile(self, file):
         """Create a new patient from a FHIR JSON file"""
-        headers = {"Accept": "application/fhir+json", "Content-Type": "application/json"}
-        r = requests.post(f"{self.fhir}/{self.base}/Patient", data=file.read(), timeout=10, headers=headers, verify=False, auth=("fhiruser", "change-password"))
+        headers = {
+            "Accept": "application/fhir+json",
+            "Content-Type": "application/json",
+        }
+        r = requests.post(
+            f"{self.fhir}/{self.base}/Patient",
+            data=file.read(),
+            timeout=10,
+            headers=headers,
+            verify=False,
+            auth=("fhiruser", "change-password"),
+        )
         print(r.text)
-        return r.status_code
+        return r
+
+    def create_patient(self, data):
+        """Create a new patient from a FHIR JSON file"""
+        headers = {
+            "Accept": "application/fhir+json",
+            "Content-Type": "application/json",
+        }
+        r = requests.post(
+            f"{self.fhir}/{self.base}/Patient",
+            data=data,
+            timeout=10,
+            headers=headers,
+            verify=False,
+            auth=("fhiruser", "change-password"),
+        )
+        print(r.text)
+        return r
 
 
 @click.command()
-@click.option("--create", default=False, is_flag=True)
 @click.option("--file", type=click.File("r"))
-def cli_options(create, file):
+def cli_options(file):
     """
     Extract command-line arguments to either create a new patient
     No arguments: exports all patients in a JSON form
     """
     client = IBMFHIRClient("https://localhost:8005", "fhir-server/api/v4")
-    if not create:
+    if file is None:
         response = client.export_patients()
         if response.status_code == 200:
             print(response.json())
     else:
-        if file is None:
-            print("Needs the --file argument")
-        else:
-            print(client.create_patient(file))
+        print(client.create_patient(file).json())
 
 
 if __name__ == "__main__":
---
+++ tools/telephone.py
@@ -0,0 +1,125 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+"""
+
+"""
+import sys
+import click
+import json
+
+sys.path.append("./clients")
+from vista_client import VistaClient
+from ibm_fhir_client import IBMFHIRClient
+from hapi_client import HapiClient
+
+config = {
+    "vista": ("http://localhost:8002", "api"),
+    "ibm": ("https://localhost:8005", "fhir-server/api/v4"),
+    "hapi": ("http://localhost:8004", "fhir"),
+}
+
+
+@click.command()
+@click.option("--file", type=click.File("r"), required=True)
+def cli_options(file):
+    """
+    Extract command-line arguments to either create a new patient
+    Step 1a: Send the Synthea-generated file through Vista
+    Step 1b: Store the Patient ID and GET /Patient/{id}
+    Step 2a: Send the response through IBM FHIR
+    Step 2b: Store the Patient ID and GET /Patient/{id}
+    Step 3a: Send response through HAPI FHIR
+    Step 3b: Store the Patient ID and GET /Patient/{id}
+    """
+
+    vista_client = VistaClient(config["vista"][0], config["vista"][1])
+    ibm_client = IBMFHIRClient(config["ibm"][0], config["ibm"][1])
+    hapi_client = HapiClient(config["hapi"][0], config["hapi"][1])
+
+    chain_terminated = False  # Set to true whenever we need to terminate the chain
+    # Step 1
+    vista_patient_id = None
+    step1a_response = vista_client.create_patient_fromfile(file)
+    if step1a_response.status_code == 201:
+        r = step1a_response.json()
+        if r["loadStatus"] == "loaded" and r.get("icn"):
+            vista_patient_id = r["icn"]
+        else:
+            chain_terminated = True
+    else:
+        chain_terminated = True
+
+    # Add a gate here to ensure that the chain is not terminated before starting 1a
+    if chain_terminated:
+        print(step1a_response.json())
+        sys.exit(1)
+
+    vista_patient_response = None
+    step1b_response = vista_client.export_patient(vista_patient_id)
+    print(step1b_response)
+    if step1b_response.status_code == 200:
+        vista_patient_response = step1b_response.json()
+    else:
+        chain_terminated = True
+
+    if chain_terminated:
+        print(step1b_response.json())
+        sys.exit(1)
+
+    # Step 2 starts here
+    step2a_response = hapi_client.create_patient(json.dumps(vista_patient_response))
+    print(step2a_response)
+    hapi_patient_id = None
+    if step2a_response.status_code == 201:
+        r = step2a_response.json()
+        hapi_patient_id = r["id"]
+    else:
+        chain_terminated = True
+
+    if chain_terminated:
+        print(step2a_response.json())
+        sys.exit(1)
+
+    hapi_patient_response = None
+    step2b_response = hapi_client.export_patient(hapi_patient_id)
+    print(step2b_response)
+    if step2b_response.status_code == 200:
+        hapi_patient_response = step2b_response.json()
+    else:
+        chain_terminated = True
+
+    if chain_terminated:
+        print(step2b_response.json())
+        sys.exit(1)
+
+    # Step 3 starts here
+    step3a_response = ibm_client.create_patient(json.dumps(hapi_patient_response))
+    print(step3a_response)
+    ibm_patient_id = None
+    if step3a_response.status_code == 201:
+        r = step3a_response.json()
+        ibm_patient_id = r["id"]
+    else:
+        chain_terminated = True
+
+    if chain_terminated:
+        print(step3a_response.json())
+        sys.exit(1)
+
+    ibm_patient_response = None
+    step3b_response = ibm_client.export_patient(ibm_patient_id)
+    print(step3b_response)
+    if step3b_response.status_code == 200:
+        ibm_patient_response = step3b_response.json()
+    else:
+        chain_terminated = True
+
+    if chain_terminated:
+        print(step3b_response.json())
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    cli_options()
