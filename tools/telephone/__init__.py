71e0f7416fc980a6ae88237ccf9bc7ce1e109098
Prashant Anantharaman <lists@prashant.at>
2024-04-19 16:07:41 -0400

    Run everything through the pre-commit hooks

--- tools/telephone/__init__.py
+++ tools/telephone/__init__.py
@@ -1,5 +1,4 @@
-import controllers.telephone as telephone
 import controllers.analyzer as analyzer
 import controllers.generator as generator
 import controllers.rabbitmq as rabbitmq
-import controllers.generator as generator
+import controllers.telephone as telephone
