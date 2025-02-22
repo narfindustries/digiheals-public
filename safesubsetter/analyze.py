0b8b0ccfd4faa81fee882882d7624f1a98f4dcf7
see-k-er <vishnupriya.varadharaju@narfindustries.com>
2025-02-21 20:45:07 -0500

    Modify resources depth table in latex

--- safesubsetter/analyze.py
+++ safesubsetter/analyze.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 
 
 def read_json_file(filename):
@@ -18,10 +19,13 @@ def generate_latex_table(count: int = -1):
     if count == -1:
         count = len(data.keys())
     df = pd.DataFrame(
-        [(k, v["max_depth"], v["max_depth_chain"]) for k, v in data.items()],
+        [
+            (k, v["max_depth"], (" $\\rightarrow$ ").join(v["max_depth_chain"]))
+            for k, v in data.items()
+        ],
         columns=["Resource", "MaxDepth", "Chain"],
     )
-    df = df.sort_values("MaxDepth", ascending=False).head(count)
+    df = df.sort_values("MaxDepth", ascending=True).head(count)
 
     # Generate LaTeX table
     latex_table = df.to_latex(
@@ -36,26 +40,26 @@ def save_latex_table(latex_table, output_file):
         f.write(latex_table)
 
 
-def generate_depth_frequency_chart():
-    """
-    Reads a JSON file containing {filename: max_depth}
-    Finds the frequency of each depth and plots a bar chart with Depths in the X axis
-    and their frequency in the Y axis
-    """
-    data = read_json_file("json_file_name")
-    depths = data.values()
+# def generate_depth_frequency_chart():
+#     """
+#     Reads a JSON file containing {filename: max_depth}
+#     Finds the frequency of each depth and plots a bar chart with Depths in the X axis
+#     and their frequency in the Y axis
+#     """
+#     data = read_json_file("json_file_name")
+#     depths = data.values()
 
-    df = pd.DataFrame(depths, columns=["depth"])
-    freq = df["depth"].value_counts().sort_index()
+#     df = pd.DataFrame(depths, columns=["depth"])
+#     freq = df["depth"].value_counts().sort_index()
 
-    ax = freq.plot(kind="bar")
-    ax.set_xlabel("Maximum Depth")
-    ax.set_ylabel("Frequency")
-    ax.set_title("Distribution of Maximum Depths")
+#     ax = freq.plot(kind="bar")
+#     ax.set_xlabel("Maximum Depth")
+#     ax.set_ylabel("Frequency")
+#     ax.set_title("Distribution of Maximum Depths")
 
-    plt.tight_layout()
-    plt.savefig("depth_frequency.png")
-    plt.close()
+#     plt.tight_layout()
+#     plt.savefig("depth_frequency.png")
+#     plt.close()
 
 
 def main():
